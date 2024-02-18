51b95d141b1ee2ef1ecc94b09315485bdfd03e0d
ryan <rhynogolfer42@gmail.com>
2024-02-17 20:24:50 -0800

    Hero or Home

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a new Software Engineer and enjoy coding. I have made projects using JavaScript, React, Next.js, Tailwind, CSS, Html, and using APIs. Projects I want to build in the future will consit of Python and MySQL.
+      </SectionText>
+
+      {/* button that can lead to my resume or even just about page. */}
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
